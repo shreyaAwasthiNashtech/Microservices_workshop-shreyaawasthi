d7fb9d05e0429309b618778f3a417ad6fffa19b4
shreyaAwasthiNashtech <shreya.awasthi@nashtechglobal.com>
2025-07-22 20:11:22 +0530

    Task-4 of Assignment 3: Set up Spring Cloud Gateway with service discovery using lb:// URIs is successfully completed

--- day3/orderservice/src/main/java/com/evolve/orderservice/controller/OrderController.java
+++ day3/orderservice/src/main/java/com/evolve/orderservice/controller/OrderController.java
@@ -2,16 +2,24 @@ package com.evolve.orderservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class OrderController {
+
+    // @GetMapping
+    // public String getOrders(@RequestParam(defaultValue = "World") String name) {
+    //     return "Hello, " + name;
+    // }
 
     @GetMapping("/orders")
     public String getOrders() {
         return "Order list from order-service";
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/order/test")
     public String hello() {
         return "Hello from order-service";
     }
--- day3/userservice/src/main/java/com/evolve/userservice/controller/UserController.java
+++ day3/userservice/src/main/java/com/evolve/userservice/controller/UserController.java
@@ -2,12 +2,24 @@ package com.evolve.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UserController {
 
+    // @GetMapping
+    // public String greetUser(@RequestParam(defaultValue = "World") String name) {
+    //     return "Hello, " + name;
+    // }
+
     @GetMapping("/users")
     public String getUsers() {
         return "User service is working!";
+    }
+
+     @GetMapping("/user/test")
+    public String hello() {
+        return "Hello from user-service";
     }
 }
